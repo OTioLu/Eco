fe2318dbed1dc3afb2cd5e967dfead7d25525a1a
magalhau <guilherme.magalhaes.grr@gmail.com>
2025-09-16 22:38:59 -0300

    camera
    ta seguindo

---
+++ jam-eco/Assets/scripts/camera_S/CameraSeguir.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSeguir : MonoBehaviour
+{
+   
+    public Transform player;   
+    public Vector3 offset;     
+
+    void LateUpdate()
+    {
+        if (player != null)
+        {
+            
+            transform.position = player.position + offset;
+        }
+    }
+}
+
